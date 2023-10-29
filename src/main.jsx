d5f19b2711226fbfd3f23a5615ef5b2932110dfa
Alvin <alvin.kang.1130@gmail.com>
2023-10-29 14:02:53 -0700

    restructured routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Courses from "./pages/Courses.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId="1042953571357-a0cmhb9v9catrqdrj293jroj5qfp0m1p.apps.googleusercontent.com">
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<App/>} />
+            <Route exact path='/courses' element={<Courses/>} />
+          </Routes>
+        </BrowserRouter>
     </React.StrictMode>
   </GoogleOAuthProvider>
 )
