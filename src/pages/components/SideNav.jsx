aa9c97a4b9857ae10650e3b7072d593c47d6cab2
Alvin <alvin.kang.1130@gmail.com>
2023-10-31 18:54:25 -0700

    delete html files

--- src/pages/components/SideNav.jsx
+++ src/pages/components/SideNav.jsx
@@ -39,8 +39,8 @@ const SideNav = () => {
             <div className="flex items-center p-2 space-x-4">
                 <img src={picture} alt="pfp" className="w-12 h-12 rounded-full dark:bg-gray-500" />
                 <div>
-                <h2 className="text-lg font-semibold">{name
-                }</h2>
+                <p className="text-lg font-semibold">{name
+                }</p>
                 <span className="flex items-center space-x-1">
                     <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">View profile</a>
                 </span>
